956a3fb3737136af449a4e266648dfec17bedea7
Yoiner Duran <duranyoiner@gmail.com>
2023-04-05 19:01:34 -0500

    deploy correct

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #para ejecutar en el azure en el despliegue
 python manage.py migrate
 gunicorn  config.wsgi
+#ejecutar en el ssh del servidor por si hay un error 500
+python manage.py collectstatic
+
